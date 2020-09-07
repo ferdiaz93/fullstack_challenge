993eca80b47194d825a84d6d74726570b3465662
ferdiaz93 <fernandodiaz1209@gmail.com>
2020-09-06 22:30:29 -0300

    navigation component added

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,41 +1,11 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-
 import './App.css';
-
-const useStyles = makeStyles({
-  root: {
-    flexGrow: 1,
-  },
-  customLabelColor: {
-    color:"#790909"
-  }
-});
+import Navigation from './components/Navigation'
 
 function App() {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event:any, newValue:any) => {
-    setValue(newValue);
-  };
   return (
-    <div className="App">
-      <Paper className={classes.root}>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          indicatorColor="primary"
-          textColor="primary"
-          centered
-        >
-          <Tab label="Modelos" classes={{textColorPrimary: classes.customLabelColor}} />
-          <Tab label="Ficha de model" />
-        </Tabs>
-      </Paper>
+    <div className="App mt-3">
+      <Navigation/>
     </div>
   );
 }
