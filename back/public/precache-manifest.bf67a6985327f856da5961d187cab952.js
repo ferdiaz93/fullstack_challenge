cb62e5b9707430092cd6a7387b2583fa0fdae901
ferdiaz93 <fernandodiaz1209@gmail.com>
2020-09-10 03:31:55 -0300

    deploy update

---
+++ back/public/precache-manifest.bf67a6985327f856da5961d187cab952.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fda7a601c9d73a973d30ca14893b9f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dd83346c51de6002583",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "f4eab94dff25f927d7ec",
+    "url": "/static/css/main.6cc9a4ab.chunk.css"
+  },
+  {
+    "revision": "7dd83346c51de6002583",
+    "url": "/static/js/2.129757a7.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.129757a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4eab94dff25f927d7ec",
+    "url": "/static/js/main.b2a990d7.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
