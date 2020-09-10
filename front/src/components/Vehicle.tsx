3103ebae5f29b54d7ea10f49b685384b4f6985c8
ferdiaz93 <fernandodiaz1209@gmail.com>
2020-09-10 20:27:48 -0300

    path fix

--- front/src/components/Vehicle.tsx
+++ front/src/components/Vehicle.tsx
@@ -38,7 +38,7 @@ function Vehicle(props:any):JSX.Element{
         <h4>{props.vehicle.brandName}</h4>
         <p>{props.vehicle.year} | {props.vehicle.price}</p>
         <div className="vehicle-container">
-          <img src={'http://localhost:8000/' + props.vehicle.main_img_url} alt=""/>
+          <img src={props.vehicle.main_img_url} alt=""/>
         </div>
         <div className="container-button">
           {!isShown ? null : (
